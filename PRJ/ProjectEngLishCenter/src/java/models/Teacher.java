35178709b2412f42d59d63962e0368558bd00567
viethung120705-lang <viethung120705@gmail.com>
2026-03-14 15:19:28 +0700

    Add files via upload

---
+++ PRJ/ProjectEngLishCenter/src/java/models/Teacher.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Teacher {
+    private int teacherId;
+    private String name;
+    private String phone;
+    private String email;
+    private String specialization;
+
+    public Teacher() {
+    }
+
+    public Teacher(int teacherId, String name, String phone, String email, String specialization) {
+        this.teacherId = teacherId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.specialization = specialization;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+    
+}
