7bbd0ee08e5af6058f00d4088ca1edcd680a9e24
TanNm715 <nmtan448@gmail.com>
2026-03-15 22:02:11 +0700

    Implement insertTeacher and insertSpecialize methods
    Added methods to insert a teacher and their specializations.

--- PRJ/ProjectEngLishCenter/src/java/dal/TeacherDAO.java
+++ PRJ/ProjectEngLishCenter/src/java/dal/TeacherDAO.java
@@ -1,52 +1,107 @@
-    /*
+/*
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-     */
-    package dal;
+ */
+package dal;
 
-    import java.sql.PreparedStatement;
-    import java.sql.ResultSet;
-    import java.util.ArrayList;
-    import java.util.List;
-    import models.Teacher;
+import java.beans.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.Teacher;
 
-    /**
-     *
-     * @author Lenovo
-     */
-    public class TeacherDAO extends DBContext {
-        PreparedStatement st; //cb cau lenh
-        ResultSet rs;// hung kq tra ve
-        public List<Teacher> getTeachers() {
-            List<Teacher> list = new ArrayList<>();
-            try{
-                String sql = "SELECT t.teacher_id,\n" +
-    "                       t.name,\n" +
-    "                       t.phone,\n" +
-    "                       t.email,\n" +
-    "                       STRING_AGG(s.specialization_name, ', ') AS specialization\n" +
-    "                FROM Teacher t\n" +
-    "                JOIN Specialize sp ON t.teacher_id = sp.teacher_id\n" +
-    "                JOIN Specialization s ON sp.specialization_id = s.specialization_id\n" +
-    "                GROUP BY t.teacher_id, t.name, t.phone, t.email";
-                st = connection.prepareStatement(sql);
-                // truyen tham so cho cau lenh sql
-                // o day k can truyen tham so vi cau lenh sql k co where
+/**
+ *
+ * @author Lenovo
+ */
+public class TeacherDAO extends DBContext {
 
-                rs = st.executeQuery(); // select
-                while(rs.next()){
-                    int teacherId = rs.getInt("teacher_id");
-                    String name = rs.getString("name");
-                    String phone = rs.getString("phone");
-                    String email = rs.getString("email");
-                    String specialization = rs.getString("specialization");
+    PreparedStatement st; //cb cau lenh
+    ResultSet rs;// hung kq tra ve
 
-                    Teacher ts = new Teacher(teacherId, name, phone, email, specialization);
-                    list.add(ts);
-                }
-                return list;
-            }catch(Exception e){
-                return null;
+    public List<Teacher> getTeachers() {
+        List<Teacher> list = new ArrayList<>();
+        try {
+            String sql = "SELECT t.teacher_id,\n"
+                    + "                       t.name,\n"
+                    + "                       t.phone,\n"
+                    + "                       t.email,\n"
+                    + "                       STRING_AGG(s.specialization_name, ', ') AS specialization\n"
+                    + "                FROM Teacher t\n"
+                    + "                LEFT JOIN Specialize sp ON t.teacher_id = sp.teacher_id\n"
+                    + "                LEFT JOIN Specialization s ON sp.specialization_id = s.specialization_id\n"
+                    + "                GROUP BY t.teacher_id, t.name, t.phone, t.email";
+            st = connection.prepareStatement(sql);
+            // truyen tham so cho cau lenh sql
+            // o day k can truyen tham so vi cau lenh sql k co where
+
+            rs = st.executeQuery(); // select
+            while (rs.next()) {
+                int teacherId = rs.getInt("teacher_id");
+                String name = rs.getString("name");
+                String phone = rs.getString("phone");
+                String email = rs.getString("email");
+                String specialization = rs.getString("specialization");
+
+                Teacher ts = new Teacher(teacherId, name, phone, email, specialization);
+                list.add(ts);
             }
+            return list;
+        } catch (Exception e) {
+            return null;
         }
     }
+
+    public int insertTeacher(String name, String phone, String email) {
+        int teacherId = -1;
+        try {
+            String sql = """
+                         INSERT INTO Teacher(name, phone, email)
+                         VALUES (?, ?, ?)
+                         """;
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, phone);
+            ps.setString(3, email);
+            ps.executeUpdate();
+
+            String sql2 = """
+                          SELECT TOP 1 teacher_id 
+                          FROM Teacher 
+                          ORDER BY teacher_id DESC
+                          """;
+            PreparedStatement ps2 = connection.prepareStatement(sql2);
+            ResultSet rs = ps2.executeQuery();
+
+            if (rs.next()) {
+                teacherId = rs.getInt("teacher_id");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            teacherId = -1;
+        }
+        return teacherId;
+    }
+    
+    public void insertSpecialize(int teacherId, String[] specIds) {
+
+    String sql = """
+                 INSERT INTO Specialize(teacher_id, specialization_id) 
+                 VALUES (?,?)
+                 """;
+    try {
+        PreparedStatement ps = connection.prepareStatement(sql);
+
+        for (String s : specIds) {
+            ps.setInt(1, teacherId);
+            ps.setInt(2, Integer.parseInt(s));
+            ps.executeUpdate();
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+}
